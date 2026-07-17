188eb3b6bd444651a1d35a6d3aa0425039ce9bc2
Alex <astro_studios@outlook.com>
2026-07-17 15:41:00 +0100

    fixed c -> cpp conversion errors and added to MPU6050 class

--- Drone/include/MPU6050.h
+++ Drone/include/MPU6050.h
@@ -16,72 +16,12 @@
 
 #define MPU_ADDR        0x68
 
-esp_err_t mpuReadfromReg (uint8_t Reg, uint8_t *ReadBuffer, size_t len)
-{
-	return (i2c_master_write_read_device(I2C_NUM, MPU_ADDR, &Reg, 1, ReadBuffer, len, 2000));
-}
-
-esp_err_t mpuWriteReg (uint8_t Reg, uint8_t data)
-{
-	uint8_t writeBuf[2];  // writeBuf[len+1];
-	writeBuf[0] = Reg;
-	writeBuf[1] = data;
-	return (i2c_master_write_to_device(I2C_NUM, MPU_ADDR, writeBuf, 2, 1000));
-}
-
-static esp_err_t i2c_master_init(void)
-{
-    int i2c_master_port = I2C_NUM;
-
-    i2c_config_t conf = {
-        .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_SDA,
-        .scl_io_num = I2C_SCL,
-        .sda_pullup_en = GPIO_PULLUP_ENABLE,
-        .scl_pullup_en = GPIO_PULLUP_ENABLE,
-        .master.clk_speed = 400000,
-    };
-
-    i2c_param_config(i2c_master_port, &conf);
-
-    return i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
-}
-
-void app_main(void)
-{
-    uint8_t data[10];
-    ESP_ERROR_CHECK(i2c_master_init());
-    ESP_LOGI(TAG, "I2C initialized successfully");
-    
-    mpuReadfromReg(0x75, data, 1);
-    ESP_LOGI(TAG, "%X", data[0]);
-    
-    mpuWriteReg(0x6B, 0);
-    mpuWriteReg(0x19, 7); // sample rate 1KHz
-    mpuWriteReg(0x1C, 0);  // ACC FS Range ±2g
-    
-    while (1)
-    {
-		mpuReadfromReg(0x3B, data, 6);
-		
-		int16_t RAWX = (data[0]<<8)|data[1];
-		int16_t RAWY = (data[2]<<8)|data[3];
-		int16_t RAWZ = (data[4]<<8)|data[5];
-		
-		float xg = (float)RAWX/16384;
-		float yg = (float)RAWY/16384;
-		float zg = (float)RAWZ/16384;
-		
-		ESP_LOGI(TAG, "\nx=%.2f\ty=%.2f\tz=%.2f", xg, yg, zg);
-		sleep(1);
-		
-	}
-}
+static const char *TAG = "MPU6050_I2C";
 
 class MPU6050{
 public:
 
-    MPU6050
+    MPU6050::MPU6050();
 
     void update();
 
@@ -91,6 +31,10 @@ public:
 
 private:
 
+    esp_err_t mpuReadfromReg (uint8_t Reg, uint8_t *ReadBuffer, size_t len);
+    esp_err_t mpuWriteReg (uint8_t Reg, uint8_t data);
+    static esp_err_t i2c_master_init(void);
+
     Vec3 getAngleFromAccel(Vec3 accel);
 
     Vec3 currentAcceleration;
@@ -98,6 +42,6 @@ private:
 
     Vec3 currentAngle;
 
+    uint8_t data[10];
 
-
-}
+};
--- Drone/src/MPU6050.cpp
+++ Drone/src/MPU6050.cpp
@@ -8,24 +8,71 @@
 
 #include "driver/i2c_master.h"
 
-MPU6050::MPU6050(){
+esp_err_t MPU6050::mpuReadfromReg (uint8_t Reg, uint8_t *ReadBuffer, size_t len)
+{
+	return (i2c_master_write_read_device(I2C_NUM, MPU_ADDR, &Reg, 1, ReadBuffer, len, 2000));
+}
 
+esp_err_t MPU6050::mpuWriteReg (uint8_t Reg, uint8_t data)
+{
+	uint8_t writeBuf[2];  // writeBuf[len+1];
+	writeBuf[0] = Reg;
+	writeBuf[1] = data;
+	return (i2c_master_write_to_device(I2C_NUM, MPU_ADDR, writeBuf, 2, 1000));
+}
+
+esp_err_t MPU6050::i2c_master_init(void)
+{
+    i2c_port_t i2c_master_port = I2C_NUM;
+
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_SDA,
+        .scl_io_num = I2C_SCL,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master = {
+            .clk_speed = 400000
+        }
+    };
+
+    i2c_param_config(i2c_master_port, &conf);
+
+    return i2c_driver_install(i2c_master_port, conf.mode, 0, 0, 0);
+}
+
+MPU6050::MPU6050(){
+    ESP_ERROR_CHECK(i2c_master_init());
+    ESP_LOGI(TAG, "I2C initialized successfully");
+
+    mpuReadfromReg(0x75, data, 1);
+    ESP_LOGI(TAG, "%X", data[0]);
+    
+    mpuWriteReg(0x6B, 0);
+    mpuWriteReg(0x19, 7); // sample rate 1KHz
+    mpuWriteReg(0x1C, 0);  // ACC FS Range ±2g
 }
 
 void MPU6050::update(){
-
-
-
+    mpuReadfromReg(0x3B, data, 6);
+		
+    int16_t RAWX = (data[0]<<8)|data[1];
+    int16_t RAWY = (data[2]<<8)|data[3];
+    int16_t RAWZ = (data[4]<<8)|data[5];
+    
+    float xg = (float)RAWX/16384;
+    float yg = (float)RAWY/16384;
+    float zg = (float)RAWZ/16384;
+    
+    ESP_LOGI(TAG, "\nx=%.2f\ty=%.2f\tz=%.2f", xg, yg, zg);
 }
 
 Vec3 MPU6050::getAngle(){
-
-
+    return currentAngle;
 }
 
 Vec3 MPU6050::getAcceleration(){
-
-
+    return currentAcceleration;
 }
 
 Vec3 MPU6050::getAngleFromAccel(Vec3 accel){
