06351c0ac83ca673c4270961ce99bcadc25cb7c8
Alex <astro_studios@outlook.com>
2026-07-17 15:31:48 +0100

    added mpu6050 code from controllerstech.com

---
+++ Drone/src/MPU6050.cpp
@@ -0,0 +1,34 @@
+#include "MPU6050.h"
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/i2c_master.h"
+#include "esp_log.h"
+
+#include "driver/i2c_master.h"
+
+MPU6050::MPU6050(){
+
+}
+
+void MPU6050::update(){
+
+
+
+}
+
+Vec3 MPU6050::getAngle(){
+
+
+}
+
+Vec3 MPU6050::getAcceleration(){
+
+
+}
+
+Vec3 MPU6050::getAngleFromAccel(Vec3 accel){
+
+
+}
